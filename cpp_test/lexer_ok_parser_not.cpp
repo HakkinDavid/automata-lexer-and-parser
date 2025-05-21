6031db85aabd80245a776815b2a9e9c463740f36
HakkinDavid <desr11@hotmail.com>
2025-05-21 01:08:47 -0700

    se ve bien

---
+++ cpp_test/lexer_ok_parser_not.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+
+int main (void) {
+    int a_ = 1;
+    int b_ = 2;
+    int c_ = 3;
+
+    std::string abc = "abc";
+    char e_ = 'e';
+
+    float o_ = 40;
+
+    long int r_ = 1.1;
+
+    double dd = 300003;
+
+    for (int i_ = 0; i_ < abc.length(); i_++) {
+        std::cout << abc[i_] << std::endl;
+    }
+}
